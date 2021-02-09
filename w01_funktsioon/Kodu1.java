b472a9ff96a11bee9a00711bb082c6c50e007982
Margen Peterson <70900361+TLUPeterson@users.noreply.github.com>
2021-02-09 08:43:51 +0200

    Update Kodu1.java

--- w01_funktsioon/Kodu1.java
+++ w01_funktsioon/Kodu1.java
@@ -1,14 +1,19 @@
 import java.text.DecimalFormat;
 
 class Kodu1 {
-    public static void main(String[] arg) {
-        DecimalFormat formatter = new DecimalFormat("#,###");
+
+    public static double kaugusMaast(double i){
         double algusAasta = 1977;
         double kiirus = 61200; //km/h
         double aastaTunnid = 8760;
-        System.out.println("Sisesta, millise aasta Voyager 1 kaugust Maast soovid vaadata: ");
+        return (i - algusAasta) * (kiirus * aastaTunnid);
+    }
+    public static void main(String[] arg) {
         double valitudAasta = Integer.parseInt(arg[0]);
-        double kaugusMaast = (valitudAasta - algusAasta) * (kiirus * aastaTunnid);
-        System.out.println("Aastal " + valitudAasta + " asub Voyager1 umbes " + formatter.format(kaugusMaast) + " km Maast");
+        DecimalFormat formatter = new DecimalFormat("#,###");
+
+        System.out.println("Sisesta, millise aasta Voyager 1 kaugust Maast soovid vaadata: ");
+
+        System.out.println("Aastal " + valitudAasta + " asub Voyager1 umbes " + formatter.format(kaugusMaast(valitudAasta)) + " km Maast");
     }
 }
